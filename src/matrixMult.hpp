a50f7269349aef603f74c476a379ead009f8374e
marcolacagnina <marcolacagnina2001@gmail.com>
2023-11-23 21:09:46 +0100

    Added naive implementation with accumulator

--- src/matrixMult.hpp
+++ src/matrixMult.hpp
@@ -9,7 +9,7 @@
  * @param right the right matrix.
  * @param result the resulting matrix.
  * @param rows number of rows of the left matrix.
- * @param inners 
+ * @param inners number of columns of the left matrix, equal to the number of rows of the right matrix.
  * @param columns number of columns of the right matrix.
 */
 template<typename T>
@@ -18,11 +18,39 @@ void naiveMMM(const T* left,const T* right, T* result, size_t rows, size_t inner
     for(size_t row = 0; row < rows; row++) {
         for(size_t col = 0; col < columns; col++) {
             for(size_t inner = 0; inner < inners; inner++) {
-                result[row * columns] += 
-                    left[inner * columns] * right[inner * columns + col];
+                result[row * columns + col] += 
+                    left[row * columns + inner] * right[inner * columns + col];
             }
         }
     }
 }
 
 
+/**
+ * Computes the multiplication between two matrices by computing the inner 
+ * product between the rows of the left matrix and the columns of the
+ * right matrix. The inner product is performed in a variable
+ * so that the update is done by accessing a register.
+ * 
+ * @tparam T the type of values contained in the matrices.
+ * 
+ * @param left the left matrix.
+ * @param right the right matrix.
+ * @param result the resulting matrix.
+ * @param rows number of rows of the left matrix.
+ * @param inners number of columns of the left matrix, equal to the number of rows of the right matrix.
+ * @param columns number of columns of the right matrix.
+*/
+template<typename T>
+void naiveAccMMM(const T* left,const T* right, T* result, size_t rows, size_t inners, size_t columns) 
+{
+    for(size_t row = 0; row < rows; row++) {
+        for(size_t col = 0; col < columns; col++) {
+            T acc = T();
+            for(size_t inner = 0; inner < inners; inner++) {
+                acc += left[row * columns + inner] * right[inner * columns + col];
+            }
+            result[row * columns + col] = acc;
+        }
+    }
+}
