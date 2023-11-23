39519bcbb0d61b84d620a8bece04179574c1c9f2
fablnt <fabio.lentini@mail.polimi.it>
2023-11-23 09:03:03 +0000

    Corrected Cmake file and sketched a documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "matrixMult.hpp"
 
 
 int main (){
--- src/matrixMult.hpp
+++ src/matrixMult.hpp
@@ -1,9 +1,23 @@
+/**
+ * Computes the multiplication between two matrices by computing the inner 
+ * product between the rows of the left matrix and the columns of the 
+ * right matrix.
+ * 
+ * @tparam T the type of values contained in the matrices.
+ * 
+ * @param left the left matrix.
+ * @param right the right matrix.
+ * @param result the resulting matrix.
+ * @param rows number of rows of the left matrix.
+ * @param inners 
+ * @param columns number of columns of the right matrix.
+*/
 template<typename T>
-void naiveMMM(T* left, T* right, T* result, size_t rows, size_t inners, size_t columns) 
+void naiveMMM(const T* left,const T* right, T* result, size_t rows, size_t inners, size_t columns) 
 {
-    for(size_t row = 0; i < rows; i++) {
-        for(size_t col = 0; j < columns) {
-            for(size_t inner = 0; k < inners; k++) {
+    for(size_t row = 0; row < rows; row++) {
+        for(size_t col = 0; col < columns; col++) {
+            for(size_t inner = 0; inner < inners; inner++) {
                 result[row * columns] += 
                     left[inner * columns] * right[inner * columns + col];
             }
