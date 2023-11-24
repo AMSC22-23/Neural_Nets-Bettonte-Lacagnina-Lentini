65c4f115ef566e39ad50dd1674646ed31b6ddf91
marcolacagnina <marcolacagnina2001@gmail.com>
2023-11-24 16:28:04 +0100

    Added Google Benchmark to main source

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <typeinfo>
+#include <benchmark/benchmark.h>
 
 #include "matrixMult.hpp"
 #include "matrixManagement.hpp"
+
+void testBench(auto* left, auto* right, auto* result, size_t rows, size_t inners, size_t columns, int unrollFactor, size_t tileSize);
 
 /**
  * Initialise randomly two matrices and call functions performing matrix-matrix multiplication
@@ -10,6 +13,7 @@
 int main (){
 
     int unrollFactor = 4;
+	  size_t tileSize = 4;
     size_t rows, inners, columns;
     
     char type;
@@ -19,7 +23,6 @@ int main (){
     std::cout << "rows: " ; std::cin >> rows; 
     std::cout << "inners: " ; std::cin >> inners;
     std::cout << "columns: "; std::cin >> columns;
-    std::cout << "Insert unrollFactor: "; std::cin >> unrollFactor; 
     
     //ask for type of elements (float or double)
     do{
@@ -27,25 +30,13 @@ int main (){
     	std::cin >> type;
     }while(type !='f' && type != 'd');
 
-
-
     //allocate two uni-dimensional arrays and fill them randomly
     if(type == 'f'){
     	auto *left = generateMatrix<float>(rows, columns);
     	auto *right = generateMatrix<float>(inners, columns);
     	float *result = new float[rows * columns];
-    	
-    	
-    	naiveMMM(left, right, result, rows, inners, columns);
-    	std::cout << std::endl;
-    	headMatrix(result, 7);
-    	//loopUnrollingMMM(left, right, result,rows, inners, columns, unrollFactor);
-    	std::cout << std::endl;
-    	headMatrix(result, 7);
-    	
-    	
-    	//delete pointers
-    	delete[] left; delete[] right; delete[] result;
+
+      testBench(left,right,result,rows,inners,columns,unrollFactor,tileSize);
     }
     
 
@@ -53,22 +44,50 @@ int main (){
     	auto *left = generateMatrix<double>(rows, inners);
     	auto *right = generateMatrix<double>(inners, columns);
     	double *result = new double[rows * columns]; 
-    	
-    	//headMatrix(left, 5);
-    	naiveMMM(left, right, result, rows, inners, columns);
-    	std::cout << std::endl;
-    	headMatrix(result, 7);
-    	//loopUnrollingMMM(left, right, result,rows, inners, columns, unrollFactor);
-    	std::cout << std::endl;
-    	headMatrix(result, 7);
-    	
-    	//delete pointers
-    	delete[] left; delete[] right; delete[] result;
+
+      testBench(left,right,result,rows,inners,columns,unrollFactor,tileSize);
     }
 
-	
-	
-    
-    
     return 0;
+}
+
+void testBench(auto* left, auto* right, auto* result, size_t rows, size_t inners, size_t columns, int unrollFactor, size_t tileSize) {
+
+
+	benchmark::RegisterBenchmark("BM_naiveMMM", [&left, &right, &result, &rows, &inners, &columns](benchmark::State& state) {
+      for (auto _ : state) {
+        naiveMMM(left, right, result, rows, inners, columns);
+      }
+    });
+
+      benchmark::RegisterBenchmark("BM_naiveAccMMM", [&left, &right, &result, &rows, &inners, &columns](benchmark::State& state) {
+      for (auto _ : state) {
+        naiveAccMMM(left, right, result, rows, inners, columns);
+      }
+    });
+
+	 benchmark::RegisterBenchmark("BM_cacheFriendlyMMM", [&left, &right, &result, &rows, &inners, &columns](benchmark::State& state) {
+      for (auto _ : state) {
+        cacheFriendlyMMM(left, right, result, rows, inners, columns);
+      }
+    });
+
+	benchmark::RegisterBenchmark("BM_loopUnrollingMMM", [&left, &right, &result, &rows, &inners, &columns, &unrollFactor](benchmark::State& state) {
+      for (auto _ : state) {
+        loopUnrollingMMM(left, right, result, rows, inners, columns, unrollFactor /*TODO*/); 
+      }
+    });
+
+	benchmark::RegisterBenchmark("BM_tilingMMM", [&left, &right, &result, &rows, &inners, &columns, &tileSize](benchmark::State& state) {
+      for (auto _ : state) {
+        tilingMMM(left, right, result, rows, inners, columns, tileSize /*TODO*/ );
+      }
+    });
+
+
+    benchmark::RunSpecifiedBenchmarks();
+    	
+    //delete pointers
+    delete[] left; delete[] right; delete[] result;
+    
 }
