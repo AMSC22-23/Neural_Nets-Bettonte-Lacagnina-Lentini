30b1872d093215de1e0d4d87f0db49d74a2ebfca
fablnt <fabio.lentini@mail.polimi.it>
2023-11-24 11:01:34 +0000

    Cache frindly implementation

--- src/matrixManagement.hpp
+++ src/matrixManagement.hpp
@@ -18,7 +18,7 @@ T* generateMatrix(size_t rows, size_t cols)
     unsigned int seed = static_cast<unsigned int>(std::chrono::duration_cast<std::chrono::milliseconds>(currentTime.time_since_epoch()).count());
     
     std::mt19937 gen(seed); // Generator based on Mersenne Twister
-    std::uniform_real_distribution<double> distribution(0.0, 100000.0); // uniformal distribution between 0 and 100k
+    std::uniform_real_distribution<T> distribution(0.0, 100000.0); // uniformal distribution between 0 and 100k
 	
 	T* matrix = new T[dimension];
 	for (int i = 0; i < dimension; ++i) {
--- src/matrixMult.hpp
+++ src/matrixMult.hpp
@@ -46,11 +46,56 @@ void naiveAccMMM(const T* left,const T* right, T* result, size_t rows, size_t in
 {
     for(size_t row = 0; row < rows; row++) {
         for(size_t col = 0; col < columns; col++) {
-            T acc = T();
+            T acc = T(); //T acc{};
             for(size_t inner = 0; inner < inners; inner++) {
                 acc += left[row * columns + inner] * right[inner * columns + col];
             }
             result[row * columns + col] = acc;
         }
     }
+}
+
+
+/**
+ * //todo
+ * 
+ * @tparam T the type of values contained in the matrices.
+ * 
+ * @param left the left matrix.
+ * @param right the right matrix.
+ * @param result the resulting matrix.
+ * @param rows number of rows of the left matrix.
+ * @param inners number of columns of the left matrix, equal to the number of rows of the right matrix.
+ * @param columns number of columns of the right matrix.
+*/
+template<typename T>
+void cacheFriendlyMMM(const T* left,const T* right, T* result, size_t rows, size_t inners, size_t columns) 
+{
+    for(size_t row = 0; row < rows; row++) {
+        for(size_t inner = 0; inner < inners; inner++) {
+            for(size_t col = 0; col < columns; col++) {
+                result[row * columns + col] += 
+                    left[row * columns + inner] * right[inner * columns + col];
+            }
+        }
+    }
+}
+
+
+/**
+ * //todo
+ * 
+ * @tparam T the type of values contained in the matrices.
+ * 
+ * @param left the left matrix.
+ * @param right the right matrix.
+ * @param result the resulting matrix.
+ * @param rows number of rows of the left matrix.
+ * @param inners number of columns of the left matrix, equal to the number of rows of the right matrix.
+ * @param columns number of columns of the right matrix.
+*/
+template<typename T>
+void tilingMMM(const T* left,const T* right, T* result, size_t rows, size_t inners, size_t columns) 
+{
+
 }
